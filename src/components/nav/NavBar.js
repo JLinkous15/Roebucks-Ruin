56e967e84612135090fe2421b52be82acfb6ebb1
James Linkous <james.william.linkous@gmail.com>
2022-12-06 17:52:01 -0600

    working navbar

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -3,48 +3,62 @@ import { FaBars } from "react-icons/fa";
 import "./NavBar.css"
 import "../../index.css"
 import { NavBarData_User } from "./NavBarData";
+import { useState } from "react";
 
 export const NavBar = ({setTheme, theme}) => {
-    const localRoebuck=localStorage.getItem("roebucks_user")
+    const[hamburger, setHamburger]=useState(false)
+    const navigate = useNavigate()
   
     return (
-        <>
-            <ul className="navbar">
+        <div className="nav">
+            <ul className={`navbar ${theme?"dark":"light"}`}>
                 <li className="navbar_item">
                     <Link className="navbar_link" to="/"><img className="navbar_image logo" alt="home" src="../../icons/Roebucksruin_Bug.svg"/></Link>
                 </li>
-                <div className="navbar right">
+                <div className="navbar_item right">
                     {theme?
                         <button className="btnDark"
                         onClick={(e)=>{setTheme(!theme)}}>
                             <img className="navbar_image toggle" 
-                            src="../../icons/darkmode_light.svg" 
+                            src="../../icons/darkmode_dark.svg" 
                             alt="moon" />
                         </button>:
                         <button className="btnDark"
                         onClick={(e)=>{setTheme(!theme)}}>
                             <img className="navbar_image toggle"
-                            src="../../icons/darkmode_dark.svg"
+                            src="../../icons/darkmode_light.svg"
                             alt="sun" />
                         </button>
                     }
                 <li className="navbar_bars">
-                    <Link className="navbar_image bars" style={theme?{color: "black"}:{color: "white"}}to="/"><FaBars /></Link>
+                    <Link className={`navbar_image bars ${theme?"dark":"light"}`} onClick={()=>{setHamburger(!hamburger)}}><FaBars /></Link>
                 </li>
                 </div>
             </ul>
             <div className="nav_menu_container">
-                <nav className="nav_menu active">
+                <nav className={`nav_menu${hamburger?"_active":""} ${theme?"dark":"light"}`}>
                     <ul className="nav_menu_list">
-                {NavBarData_User.map((listItem, index)=>{
-                        return (
-                            <li key={index}><Link className={theme?"nav-text light":"nav-text dark"} to={listItem.path}>{listItem.icon}{listItem.title}</Link></li>
-                        )
-                    })}
+                        {NavBarData_User.map((listItem, index)=>{
+                                return (
+                                    <li className="nav_menu_content" key={index}>
+                                        <Link className={theme?"nav-text dark":"nav-text light"} to={listItem.path} 
+                                        onClick={()=>{setHamburger(!hamburger)}}>
+                                            {listItem.icon} {listItem.title}
+                                        </Link>
+                                    </li>
+                                )
+                            })}
+                    <button className={`btn ${theme?"light":"dark"}`}
+                    onClick={()=>{
+                        localStorage.removeItem("roebucks_user", { replace : true })
+                        navigate("/")
+                    }}>
+                        Logout
+                    </button>
                     </ul>
                 </nav>
             </div>
-        </>
+        </div>
     )
 }
 
