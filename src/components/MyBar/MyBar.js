016fa7ba14346461c4fadf85f978083aba6899f4
James Linkous <james.william.linkous@gmail.com>
2022-12-23 16:00:17 -0600

    power outage backup

--- src/components/MyBar/MyBar.js
+++ src/components/MyBar/MyBar.js
@@ -6,15 +6,37 @@ export const MyBar = ({theme, hamburger, setHamburger, setMyBarMenu}) => {
     const localUser = localStorage.getItem("roebucks_user")
     const localUserObj = JSON.parse(localUser)
 
+    const [firstUserCocktail, setFirstUserCocktail] = useState({})
     const [userCocktails, setUserCocktails] = useState([])
 
     useEffect(()=>{
         fetch(`http://localhost:8088/cocktails?userId=${localUserObj.id}`)
         .then(res=>res.json())
-        .then(setUserCocktails)
+        .then((res)=>{
+            const copy = res.sort((a, b)=>{
+                if(a.id > b.id){
+                    return -1
+                }else if (a.id < b.id){
+                    return 1 
+                }
+                return 0})
+                const firstElement = copy.shift()
+            setFirstUserCocktail(firstElement)
+            setUserCocktails(copy)
+        })    
     },[])
+
     return <section className={`mybar componentContainer ${theme?"light":"dark"}`} onClick={(e)=>{setHamburger(true)
         setMyBarMenu(true)}}>
+
+            <div className="firstUserCocktail">
+                <Link to={`/mybar/${firstUserCocktail.id}/view`} style={{backgroundImage: `url(${firstUserCocktail.image})`}}>
+                    <h1>{firstUserCocktail.name}</h1>
+                    <h2>{firstUserCocktail.dateCompleted}</h2>
+                </Link>
+                <ul></ul>
+            </div>
+
             <div className="mybar_cocktails">
             {userCocktails.map((cocktail, index)=>{
                 return <Link key={index} 
@@ -24,5 +46,6 @@ export const MyBar = ({theme, hamburger, setHamburger, setMyBarMenu}) => {
 
                 })}
         </div>
+
     </section>
 }
